4bf97773cc951f23fed84cb143587ed2894651a0
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2021-09-22 12:35:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Azure-WriteTo-SATable
+
+![GitHub repo size](https://img.shields.io/github/repo-size/niklasrast/Azure-WriteTo-SATable)
+
+![GitHub issues](https://img.shields.io/github/issues-raw/niklasrast/Azure-WriteTo-SATable)
+
+![GitHub last commit](https://img.shields.io/github/last-commit/niklasrast/Azure-WriteTo-SATable)
+
+This repo contains an powershell scripts to write values from PowerShell to an Azure Storage Account Table.
+The Storage Account that you will use must be accessible from your network or the internet.
+
+## Install:
+```powershell
+PowerShell.exe -ExecutionPolicy Bypass -Command .\Azure-WriteTo-SATable.ps1
+```
+
+## Script Parameter definitions:
+```powershell
+$AzureEndpoint = 'https://<STORAGEACCOUNTNAME>.table.core.windows.net'
+$AzureSharedAccessSignature  = '<SASTOKEN>'
+$AzureTable = "<TABLENAME>"
+```
+Replace <STORAGEACCOUNTNAME>, <SASTOKEN> and <TABLENAME> to your informations.
+
+## Script Value definitions:
+```powershell
+Add-Member -InputObject $TableObject -Membertype NoteProperty -Name "<KEYNAME>" -Value (<VALUE>).ToString();
+```
+Replace <KEYNAME> and <VALUE> to your values.
+
+
+## Requirements:
+- PowerShell 5.0
+- Windows 10
+
+# Feature requests
+If you have an idea for a new feature in this repo, send me an issue with the subject Feature request and write your suggestion in the text. I will then check the feature and implement it if necessary.
+
+Created by @niklasrast 
---
+++ WriteTo-AzureTable.ps1
@@ -0,0 +1,121 @@
+﻿#Requires -Version 3.0
+<#
+    .SYNOPSIS 
+    Windows 10 Software packaging wrapper
+
+    .DESCRIPTION
+    Install:   PowerShell.exe -ExecutionPolicy Bypass -Command .\WriteTo-AzureTable.ps1
+
+    .ENVIRONMENT
+    PowerShell 5.0
+
+    .AUTHOR
+    Niklas Rast
+#>
+
+$AzureEndpoint = 'https://<STORAGEACCOUNTNAME>.table.core.windows.net'
+$AzureSharedAccessSignature  = '<SASTOKEN>'
+$AzureTable = "<TABLENAME>"
+
+Function Test-InternetConnection
+{
+    [CmdletBinding()]
+    Param
+    (
+        [parameter(Mandatory=$true)][string]$Target
+    )
+
+    $Result = Test-NetConnection -ComputerName ($Target -replace "https://","") -Port 443 -WarningAction SilentlyContinue;
+    Return $Result;
+}
+
+Function Add-AzureTableData
+{
+    [CmdletBinding()]
+    Param
+    (
+        [parameter(Mandatory=$true)][string]$Endpoint,
+        [parameter(Mandatory=$true)][string]$SharedAccessSignature,
+        [parameter(Mandatory=$true)][string]$Table,
+        [parameter(Mandatory=$true)][hashtable]$TableData
+    )
+
+    $Headers = @{
+        "x-ms-date"=(Get-Date -Format r);
+        "x-ms-version"="2016-05-31";
+        "Accept-Charset"="UTF-8";
+        "DataServiceVersion"="3.0;NetFx";
+        "MaxDataServiceVersion"="3.0;NetFx";
+        "Accept"="application/json;odata=nometadata"
+    };
+
+    $URI
+    $URI = ($Endpoint + "/" + $Table + "/" + $SharedAccessSignature);
+
+    #Convert table data to JSON and encode to UTF8.
+    $Body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject $TableData));
+
+    #Insert data to Azure storage table.
+    Invoke-WebRequest -Method Post -Uri $URI -Headers $Headers -Body $Body -ContentType "application/json" -UseBasicParsing | Out-Null;
+}
+
+Function ConvertTo-HashTable
+{
+    [cmdletbinding()]
+    Param
+    (
+        [Parameter(Position=0,Mandatory=$True,ValueFromPipeline=$True)]
+        [object]$InputObject,
+        [switch]$NoEmpty
+    )
+     
+    Process
+    {
+        #Get propery names.
+        $Names = $InputObject | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name;
+
+        #Define an empty hash table.
+        $Hash = @{};
+
+        #Go through the list of names and add each property and value to the hash table.
+        $Names | ForEach-Object {$Hash.Add($_,$InputObject.$_)};
+
+        #If NoEmpty is set.
+        If ($NoEmpty)
+        {
+            #Define a new hash.
+            $Defined = @{};
+
+            #Get items from $hash that have values and add to $Defined.
+            $Hash.Keys | ForEach-Object {
+                #If hash item is not empty.
+                If ($Hash.item($_))
+                {
+                    #Add to hashtable.
+                    $Defined.Add(($_,$Hash.Item($_)));
+                }
+            }       
+            #Return hashtable.
+            Return $Defined;
+        }
+        #Return hashtable.
+        Return $Hash;
+    }
+}
+
+If(!((Test-InternetConnection -Target $AzureEndpoint).TcpTestSucceeded -eq "true"))
+{
+    Write-Host "Cannot access the storage account through network problems."
+    Exit 1;
+}
+
+#Create a new object.
+$TableObject = New-Object -TypeName PSObject;
+Add-Member -InputObject $TableObject -Membertype NoteProperty -Name "PartitionKey" -Value ((Get-Random -Minimum 000000 -Maximum 999999)).ToString();
+Add-Member -InputObject $TableObject -Membertype NoteProperty -Name "RowKey" -Value ($ENV:COMPUTERNAME).ToString();
+
+###Add values to the object here
+Add-Member -InputObject $TableObject -Membertype NoteProperty -Name "SerialNumber" -Value ((Get-WmiObject win32_bios).SerialNumber).ToString();
+
+#Insert data to the Azure table.
+Add-AzureTableData -Endpoint $AzureEndpoint -SharedAccessSignature $AzureSharedAccessSignature -Table $AzureTable -TableData (ConvertTo-HashTable -InputObject $TableObject);
